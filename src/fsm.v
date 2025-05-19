3abf11f93bc703e0f52cca1c3f407e8d40ceab20
Edmond <mredmondwang@gmail.com>
2025-05-19 13:18:20 -0700

    Initial Commit

---
+++ src/fsm.v
@@ -0,0 +1,6 @@
+
+// fsm.v
+// --------
+// INIT -> PLAY -> WAIT -> CHECK -> ERROR
+// Generate control signals for ROM write&reads, drives LEDs
+
---
+++ src/sequence_loader.v
@@ -0,0 +1,3 @@
+// sequence_loader.v
+// --------
+// Pumps N random values into seq ROM, then asserts "done".
